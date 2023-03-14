60a2a81ddf18df52a76170c948c77d35e1c03dec
Emma Doyle <emma@sbevision.com>
2023-03-14 19:14:40 -0400

    extract-scripts: Switch to env vars

--- src/scripts/filter.sh
+++ src/scripts/filter.sh
@@ -4,15 +4,15 @@
 # shellcheck disable=SC2288,SC2001
 
 
-if ! "<< parameters.circle-token >>"; then
+if ! "$SH_CIRCLE_TOKEN"; then
     printf "Must set CircleCI token for successful authentication.\\n" >&2
     exit 1
 fi
 
-# Add each module to `modules-filtered` if 1) `force-all` is set to `true`, or 2) there is a diff against master at HEAD, or 3) no workflow runs have occurred on the default branch for this project in the past << parameters.reporting-window >> days.
-if ! "<< parameters.force-all >>" && [ "$(curl -s --request GET --url "https://circleci.com/api/v2/insights/<< parameters.project-type >>/<< parameters.circle-organization >>/${CIRCLE_PROJECT_REPONAME}/workflows?reporting-window=<< parameters.reporting-window >>" --header "Circle-Token: << parameters.circle-token >>" | '[ .items[].name ] | length')" -eq 0 ]; then
+# Add each module to `modules-filtered` if 1) `force-all` is set to `true`, or 2) there is a diff against master at HEAD, or 3) no workflow runs have occurred on the default branch for this project in the past $SH_REPORTING_WINDOW days.
+if ! "$SH_FORCE_ALL" && [ "$(curl -s --request GET --url "https://circleci.com/api/v2/insights/$SH_PROJECT_TYPE/$SH_CIRCLE_ORGANIZATION/${CIRCLE_PROJECT_REPONAME}/workflows?reporting-window=$SH_REPORTING_WINDOW" --header "Circle-Token: $SH_CIRCLE_TOKEN" | '[ .items[].name ] | length')" -eq 0 ]; then
     printf "Running all workflows.\\n"
-    echo "<< parameters.modules >>" | awk NF | while read -r module; do
+    echo "$SH_MODULES" | awk NF | while read -r module; do
         module_dots="$(sed 's@\/@\.@g' <<< "$module")"
         if [ "${#module_dots}" -gt 1 ] && [ "${module_dots::1}" = "." ]; then
             module_dots="${module_dots:1}"
@@ -21,11 +21,11 @@ if ! "<< parameters.force-all >>" && [ "$(curl -s --request GET --url "https://c
             module_dots="${module_dots::-1}"
         fi
 
-        echo "$module_dots" >> "<< parameters.modules-filtered >>"
+        echo "$module_dots" >> "$SH_MODULES_FILTERED"
     done
 else
-    pip install wildmatch=="<< parameters.wildmatch-version >>"
-    echo "<< parameters.modules >>" | awk NF | while read -r module; do
+    pip install wildmatch=="$SH_WILDMATCH_VERSION"
+    echo "$SH_MODULES" | awk NF | while read -r module; do
         module_dots="$(sed 's@\/@\.@g' <<< "$module")"
         if [ "${#module_dots}" -gt 1 ] && [ "${module_dots::1}" = "." ]; then
             module_dots="${module_dots:1}"
@@ -44,18 +44,18 @@ else
 
         # Handle root module "."
         if [ "${module_dots}" = "." ]; then
-            if [ ! -f .circleci/"<< parameters.root-config >>.ignore" ]; then
-                touch .circleci/"<< parameters.root-config >>.ignore"
+            if [ ! -f .circleci/"$SH_ROOT_CONFIG.ignore" ]; then
+                touch .circleci/"$SH_ROOT_CONFIG.ignore"
             fi
 
-            if [ "$CIRCLE_BRANCH" = "<< parameters.default-branch >>" ]; then
-                if "<< parameters.force-all >>" || [ "$(git diff-tree --no-commit-id --name-only -r HEAD~"<< parameters.squash-merge-lookbehind >>" "<< parameters.default-branch >>" "$module_dots" | awk NF | wildmatch -c ".circleci/<< parameters.root-config >>.ignore")" != "" ] || ([ "$(git diff-tree --no-commit-id --name-only -r HEAD~"<< parameters.squash-merge-lookbehind >>" "<< parameters.default-branch >>" ".circleci/<< parameters.root-config >>.yml" | awk NF)" != "" ] && "<< parameters.include-config-changes >>"); then
-                    echo "$module_dots" >> "<< parameters.modules-filtered >>"
+            if [ "$CIRCLE_BRANCH" = "$SH_DEFAULT_BRANCH" ]; then
+                if "$SH_FORCE_ALL" || [ "$(git diff-tree --no-commit-id --name-only -r HEAD~"$SH_SQUASH_MERGE_LOOKBEHIND >>" "$SH_DEFAULT_BRANCH" "$module_dots" | awk NF | wildmatch -c ".circleci/$SH_ROOT_CONFIG.ignore")" != "" ] || ([ "$(git diff-tree --no-commit-id --name-only -r HEAD~"$SH_SQUASH_MERGE_LOOKBEHIND >>" "$SH_DEFAULT_BRANCH" ".circleci/$SH_ROOT_CONFIG.yml" | awk NF)" != "" ] && "$SH_INCLUDE_CONFIG_CHANGES"); then
+                    echo "$module_dots" >> "$SH_MODULES_FILTERED"
                     printf "%s\\n" "$module_slashes"
                 fi
             else
-                if "<< parameters.force-all >>" || [ "$(git diff-tree --no-commit-id --name-only -r HEAD "<< parameters.default-branch >>" "$module_dots" | awk NF | wildmatch -c ".circleci/<< parameters.root-config >>.ignore")" != "" ] || ([ "$(git diff-tree --no-commit-id --name-only -r HEAD "<< parameters.default-branch >>" ".circleci/<< parameters.root-config >>.yml" | awk NF)" != "" ] && "<< parameters.include-config-changes >>"); then
-                    echo "$module_dots" >> "<< parameters.modules-filtered >>"
+                if "$SH_FORCE_ALL" || [ "$(git diff-tree --no-commit-id --name-only -r HEAD "$SH_DEFAULT_BRANCH" "$module_dots" | awk NF | wildmatch -c ".circleci/$SH_ROOT_CONFIG.ignore")" != "" ] || ([ "$(git diff-tree --no-commit-id --name-only -r HEAD "$SH_DEFAULT_BRANCH" ".circleci/$SH_ROOT_CONFIG.yml" | awk NF)" != "" ] && "$SH_INCLUDE_CONFIG_CHANGES"); then
+                    echo "$module_dots" >> "$SH_MODULES_FILTERED"
                     printf "%s\\n" "$module_slashes"
                 fi
             fi
@@ -68,14 +68,14 @@ else
             touch ".circleci/${module_dots}.ignore"
         fi
 
-        if [ "$CIRCLE_BRANCH" = "<< parameters.default-branch >>" ]; then
-            if "<< parameters.force-all >>" || [ "$(git diff-tree --no-commit-id --name-only -r HEAD~"<< parameters.squash-merge-lookbehind >>" "<< parameters.default-branch >>" "$module_slashes" | awk NF | wildmatch -c ".circleci/${module_dots}.ignore")" != "" ] || ([ "$(git diff-tree --no-commit-id --name-only -r HEAD~"<< parameters.squash-merge-lookbehind >>" "<< parameters.default-branch >>" .circleci/"$module_dots".yml | awk NF)" != "" ] && "<< parameters.include-config-changes >>"); then
-                echo "$module_dots" >> "<< parameters.modules-filtered >>"
+        if [ "$CIRCLE_BRANCH" = "$SH_DEFAULT_BRANCH" ]; then
+            if "$SH_FORCE_ALL" || [ "$(git diff-tree --no-commit-id --name-only -r HEAD~"$SH_SQUASH_MERGE_LOOKBEHIND >>" "$SH_DEFAULT_BRANCH" "$module_slashes" | awk NF | wildmatch -c ".circleci/${module_dots}.ignore")" != "" ] || ([ "$(git diff-tree --no-commit-id --name-only -r HEAD~"$SH_SQUASH_MERGE_LOOKBEHIND >>" "$SH_DEFAULT_BRANCH" .circleci/"$module_dots".yml | awk NF)" != "" ] && "$SH_INCLUDE_CONFIG_CHANGES"); then
+                echo "$module_dots" >> "$SH_MODULES_FILTERED"
                 printf "%s\\n" "$module_slashes"
             fi
         else
-            if "<< parameters.force-all >>" || [ "$(git diff-tree --no-commit-id --name-only -r HEAD "<< parameters.default-branch >>" "$module_slashes" | awk NF | wildmatch -c ".circleci/${module_dots}.ignore")" != "" ] || ([ "$(git diff-tree --no-commit-id --name-only -r HEAD "<< parameters.default-branch >>" .circleci/"$module_dots".yml | awk NF)" != "" ] && "<< parameters.include-config-changes >>"); then
-                echo "$module_dots" >> "<< parameters.modules-filtered >>"
+            if "$SH_FORCE_ALL" || [ "$(git diff-tree --no-commit-id --name-only -r HEAD "$SH_DEFAULT_BRANCH" "$module_slashes" | awk NF | wildmatch -c ".circleci/${module_dots}.ignore")" != "" ] || ([ "$(git diff-tree --no-commit-id --name-only -r HEAD "$SH_DEFAULT_BRANCH" .circleci/"$module_dots".yml | awk NF)" != "" ] && "$SH_INCLUDE_CONFIG_CHANGES"); then
+                echo "$module_dots" >> "$SH_MODULES_FILTERED"
                 printf "%s\\n" "$module_slashes"
             fi
         fi
