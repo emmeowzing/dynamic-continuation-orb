33d6091967846166f32054c42381c31024d54914
Emma Doyle <emma@premiscale.com>
2023-03-14 21:55:51 -0400

    repo-updates: Update repo settings (#32)
    * repo-updates: Update repo settings
    
    * repo-updates: Update sentence
    
    * repo-updates: Add review job
    
    * Extract scripts (#33)
    
    * extract-scripts: Extract scripts for shellchecking
    
    * extract-scripts: Further extract reduce
    
    * extract-scripts: Fix indentation
    
    * extract-scripts:
    
    * extract-scripts: De-flatten
    
    * extract-scripts: Validate
    
    * extract-scripts: Simplifications
    
    * extract-scripts: Switch to env vars
    
    * extract-scripts: Finish off reduce
    
    * extract-scripts: Remove redundant shebangs
    
    * extract-scripts: Fix script length
    
    * extract-scripts: Fix
    
    * extract-scripts:
    
    * extract-scripts: Remove
    
    * extract-scripts: Reverse the logic
    
    * extract-scripts: Rerun workflow, CCI is down
    
    * extract-scripts: Update string
    
    ---------
    
    Co-authored-by: Emma Doyle <emma@sbevision.com>
    
    * repo-updates: installed stale bot
    
    * repo-updates: Add orb validation hook back
    
    ---------
    
    Co-authored-by: Emma Doyle <emma@sbevision.com>

---
+++ src/scripts/reduce.sh
@@ -0,0 +1,21 @@
+# shellcheck disable=SC2288,SC2001,SC2148
+
+
+# If `modules` is unavailable, stop this job without continuation
+if [ ! -f "$SH_MODULES" ] || [ ! -s "$SH_MODULES" ]; then
+    printf "Nothing to merge. Halting the job.\\n"
+    circleci-agent step halt
+    exit 0
+fi
+
+# Convert a list of dirs to a list of config files under .circleci/.
+awk '{
+    if ($0 ~ /^\.$/) {
+        printf ".circleci/${SH_ROOT_CONFIG}.yml\n"
+    } else {
+        printf(".circleci/%s.yml\n", $0)
+    }
+}' "$SH_MODULES" > /tmp/"$CIRCLE_WORKFLOW_ID.txt"
+mv /tmp/"$CIRCLE_WORKFLOW_ID.txt" "$SH_MODULES"
+
+xargs -a "$SH_MODULES" yq -y -s "reduce .[] as \$item ({}; . * \$item)" | tee "$SH_CONTINUE_CONFIG"
