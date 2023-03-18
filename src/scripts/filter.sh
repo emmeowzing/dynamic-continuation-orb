54f01a4578b4e3eecba23680d50364c53b3dbc2d
Emma Doyle <emma@premiscale.com>
2023-03-18 12:14:06 -0400

    ENG-138: Fix filtered modules (#27)
    Minor fixes / clarifications in filtering
    Add gitlab VCS type support

--- src/scripts/filter.sh
+++ src/scripts/filter.sh
@@ -1,13 +1,25 @@
-# shellcheck disable=SC2288,SC2001,SC2148
+# shellcheck disable=SC2288,SC2001,SC2148,SC2153
 
+# Parse environment variables referencing env vars set by CircleCI.
+if [ "${SH_CIRCLE_TOKEN:0:1}" = '$' ]; then
+    _CIRCLE_TOKEN="$(eval echo "$SH_CIRCLE_TOKEN")"
+else
+    _CIRCLE_TOKEN="$SH_CIRCLE_TOKEN"
+fi
+if [ "${SH_CIRCLE_ORGANIZATION:0:1}" = '$' ]; then
+    _CIRCLE_ORGANIZATION="$(eval echo "$SH_CIRCLE_ORGANIZATION")"
+else
+    _CIRCLE_ORGANIZATION="$SH_CIRCLE_ORGANIZATION"
+fi
 
-if ! "$SH_CIRCLE_TOKEN"; then
+# CircleCI API token should be set.
+if [ -z "$_CIRCLE_TOKEN" ]; then
     printf "Must set CircleCI token for successful authentication.\\n" >&2
     exit 1
 fi
 
 # Add each module to `modules-filtered` if 1) `force-all` is set to `true`, or 2) there is a diff against master at HEAD, or 3) no workflow runs have occurred on the default branch for this project in the past $SH_REPORTING_WINDOW days.
-if ! "$SH_FORCE_ALL" && [ "$(curl -s --request GET --url "https://circleci.com/api/v2/insights/$SH_PROJECT_TYPE/$SH_CIRCLE_ORGANIZATION/${CIRCLE_PROJECT_REPONAME}/workflows?reporting-window=$SH_REPORTING_WINDOW" --header "Circle-Token: $SH_CIRCLE_TOKEN" | '[ .items[].name ] | length')" -eq 0 ]; then
+if [ ! "$SH_FORCE_ALL" ] || { [ "$SH_REPORTING_WINDOW" != "" ] && [ "$(curl -s -X GET --url "https://circleci.com/api/v2/insights/${SH_PROJECT_TYPE}/${_CIRCLE_ORGANIZATION}/${CIRCLE_PROJECT_REPONAME}/workflows?reporting-window=${SH_REPORTING_WINDOW}" --header "Circle-Token: ${SH_CIRCLE_TOKEN}" | jq -r "[ .items[].name ] | length")" -eq "0" ]; }; then
     printf "Running all workflows.\\n"
     echo "$SH_MODULES" | awk NF | while read -r module; do
         module_dots="$(sed 's@\/@\.@g' <<< "$module")"
@@ -46,12 +58,12 @@ else
             fi
 
             if [ "$CIRCLE_BRANCH" = "$SH_DEFAULT_BRANCH" ]; then
-                if "$SH_FORCE_ALL" || [ "$(git diff-tree --no-commit-id --name-only -r HEAD~"$SH_SQUASH_MERGE_LOOKBEHIND >>" "$SH_DEFAULT_BRANCH" "$module_dots" | awk NF | wildmatch -c ".circleci/$SH_ROOT_CONFIG.ignore")" != "" ] || ([ "$(git diff-tree --no-commit-id --name-only -r HEAD~"$SH_SQUASH_MERGE_LOOKBEHIND >>" "$SH_DEFAULT_BRANCH" ".circleci/$SH_ROOT_CONFIG.yml" | awk NF)" != "" ] && "$SH_INCLUDE_CONFIG_CHANGES"); then
+                if [ ! "$SH_FORCE_ALL" ] || [ "$(git diff-tree --no-commit-id --name-only -r HEAD~"$SH_SQUASH_MERGE_LOOKBEHIND >>" "$SH_DEFAULT_BRANCH" "$module_dots" | awk NF | wildmatch -c ".circleci/$SH_ROOT_CONFIG.ignore")" != "" ] || ([ "$(git diff-tree --no-commit-id --name-only -r HEAD~"$SH_SQUASH_MERGE_LOOKBEHIND >>" "$SH_DEFAULT_BRANCH" ".circleci/$SH_ROOT_CONFIG.yml" | awk NF)" != "" ] && "$SH_INCLUDE_CONFIG_CHANGES"); then
                     echo "$module_dots" >> "$SH_MODULES_FILTERED"
                     printf "%s\\n" "$module_slashes"
                 fi
             else
-                if "$SH_FORCE_ALL" || [ "$(git diff-tree --no-commit-id --name-only -r HEAD "$SH_DEFAULT_BRANCH" "$module_dots" | awk NF | wildmatch -c ".circleci/$SH_ROOT_CONFIG.ignore")" != "" ] || ([ "$(git diff-tree --no-commit-id --name-only -r HEAD "$SH_DEFAULT_BRANCH" ".circleci/$SH_ROOT_CONFIG.yml" | awk NF)" != "" ] && "$SH_INCLUDE_CONFIG_CHANGES"); then
+                if [ ! "$SH_FORCE_ALL" ] || [ "$(git diff-tree --no-commit-id --name-only -r HEAD "$SH_DEFAULT_BRANCH" "$module_dots" | awk NF | wildmatch -c ".circleci/$SH_ROOT_CONFIG.ignore")" != "" ] || ([ "$(git diff-tree --no-commit-id --name-only -r HEAD "$SH_DEFAULT_BRANCH" ".circleci/$SH_ROOT_CONFIG.yml" | awk NF)" != "" ] && "$SH_INCLUDE_CONFIG_CHANGES"); then
                     echo "$module_dots" >> "$SH_MODULES_FILTERED"
                     printf "%s\\n" "$module_slashes"
                 fi
@@ -66,12 +78,12 @@ else
         fi
 
         if [ "$CIRCLE_BRANCH" = "$SH_DEFAULT_BRANCH" ]; then
-            if "$SH_FORCE_ALL" || [ "$(git diff-tree --no-commit-id --name-only -r HEAD~"$SH_SQUASH_MERGE_LOOKBEHIND >>" "$SH_DEFAULT_BRANCH" "$module_slashes" | awk NF | wildmatch -c ".circleci/${module_dots}.ignore")" != "" ] || ([ "$(git diff-tree --no-commit-id --name-only -r HEAD~"$SH_SQUASH_MERGE_LOOKBEHIND >>" "$SH_DEFAULT_BRANCH" .circleci/"$module_dots".yml | awk NF)" != "" ] && "$SH_INCLUDE_CONFIG_CHANGES"); then
+            if [ ! "$SH_FORCE_ALL" ] || [ "$(git diff-tree --no-commit-id --name-only -r HEAD~"$SH_SQUASH_MERGE_LOOKBEHIND >>" "$SH_DEFAULT_BRANCH" "$module_slashes" | awk NF | wildmatch -c ".circleci/${module_dots}.ignore")" != "" ] || ([ "$(git diff-tree --no-commit-id --name-only -r HEAD~"$SH_SQUASH_MERGE_LOOKBEHIND >>" "$SH_DEFAULT_BRANCH" .circleci/"$module_dots".yml | awk NF)" != "" ] && "$SH_INCLUDE_CONFIG_CHANGES"); then
                 echo "$module_dots" >> "$SH_MODULES_FILTERED"
                 printf "%s\\n" "$module_slashes"
             fi
         else
-            if "$SH_FORCE_ALL" || [ "$(git diff-tree --no-commit-id --name-only -r HEAD "$SH_DEFAULT_BRANCH" "$module_slashes" | awk NF | wildmatch -c ".circleci/${module_dots}.ignore")" != "" ] || ([ "$(git diff-tree --no-commit-id --name-only -r HEAD "$SH_DEFAULT_BRANCH" .circleci/"$module_dots".yml | awk NF)" != "" ] && "$SH_INCLUDE_CONFIG_CHANGES"); then
+            if [ ! "$SH_FORCE_ALL" ] || [ "$(git diff-tree --no-commit-id --name-only -r HEAD "$SH_DEFAULT_BRANCH" "$module_slashes" | awk NF | wildmatch -c ".circleci/${module_dots}.ignore")" != "" ] || ([ "$(git diff-tree --no-commit-id --name-only -r HEAD "$SH_DEFAULT_BRANCH" .circleci/"$module_dots".yml | awk NF)" != "" ] && "$SH_INCLUDE_CONFIG_CHANGES"); then
                 echo "$module_dots" >> "$SH_MODULES_FILTERED"
                 printf "%s\\n" "$module_slashes"
             fi
--- src/scripts/reduce.sh
+++ src/scripts/reduce.sh
@@ -1,21 +1,34 @@
-# shellcheck disable=SC2288,SC2001,SC2148
+# shellcheck disable=SC2288,SC2001,SC2148,SC2002,SC2016,SC2046
+
+
+shopt -s nullglob
 
 
 # If `modules` is unavailable, stop this job without continuation
-if [ ! -f "$SH_MODULES" ] || [ ! -s "$SH_MODULES" ]; then
+if [ ! -f "$SH_MODULES_FILTERED" ] || [ ! -s "$SH_MODULES_FILTERED" ]; then
     printf "Nothing to merge. Halting the job.\\n"
     circleci-agent step halt
     exit 0
 fi
 
 # Convert a list of dirs to a list of config files under .circleci/.
-awk '{
-    if ($0 ~ /^\.$/) {
-        printf ".circleci/${SH_ROOT_CONFIG}.yml\n"
+awk "{
+    if (\$0 ~ /^\\.\$/) {
+        printf \".circleci/${SH_ROOT_CONFIG}.yml\\n\"
     } else {
-        printf(".circleci/%s.yml\n", $0)
+        printf(\".circleci/%s.yml\n\", \$0)
     }
-}' "$SH_MODULES" > /tmp/"$CIRCLE_WORKFLOW_ID.txt"
-mv /tmp/"$CIRCLE_WORKFLOW_ID.txt" "$SH_MODULES"
+}" "$SH_MODULES_FILTERED" > /tmp/"$CIRCLE_WORKFLOW_ID.txt"
+mv /tmp/"$CIRCLE_WORKFLOW_ID.txt" "$SH_MODULES_FILTERED"
 
-xargs -a "$SH_MODULES" yq -y -s "reduce .[] as \$item ({}; . * \$item)" | tee "$SH_CONTINUE_CONFIG"
+# Move yaml files -> yml so we can handle both extensions for YAML configs. Not that we want both, but we should handle both cases.
+for f in .circleci/*.yaml; do
+    printf "Migrating pipeline \"%s\" -> \"%s\"\\n" "$f" "${f%.*}.yml" >&2
+    if [ -f "${f%.*}.yml" ]; then
+        printf "ERROR: Could not migrate \"%s\", \"%s\" already exists.\\n" "$f" "${f%.*}.yml" >&2
+        exit 1
+    fi
+    mv "$f" "${f%.*}.yml"
+done
+
+yq eval-all '. as $item ireduce ( {}; . * $item )' $(cat "$SH_MODULES_FILTERED" | xargs) | tee "$SH_CONTINUE_CONFIG"
