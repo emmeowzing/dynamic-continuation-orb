8215aafed81440ec189ec7edcd6951a58fc345e0
Emma Doyle <emma@sbevision.com>
2023-03-18 11:20:41 -0400

    ENG-138: Fix reduce

--- src/scripts/reduce.sh
+++ src/scripts/reduce.sh
@@ -2,20 +2,20 @@
 
 
 # If `modules` is unavailable, stop this job without continuation
-if [ ! -f "$SH_MODULES" ] || [ ! -s "$SH_MODULES" ]; then
+if [ ! -f "$SH_MODULES_FILTERED" ] || [ ! -s "$SH_MODULES_FILTERED" ]; then
     printf "Nothing to merge. Halting the job.\\n"
     circleci-agent step halt
     exit 0
 fi
 
 # Convert a list of dirs to a list of config files under .circleci/.
-awk '{
-    if ($0 ~ /^\.$/) {
-        printf ".circleci/${SH_ROOT_CONFIG}.yml\n"
+awk "{
+    if ($0 ~ /^\\.$/) {
+        printf \".circleci/${SH_ROOT_CONFIG}.yml\\n\"
     } else {
-        printf(".circleci/%s.yml\n", $0)
+        printf(\".circleci/%s.yml\n\", $0)
     }
-}' "$SH_MODULES" > /tmp/"$CIRCLE_WORKFLOW_ID.txt"
-mv /tmp/"$CIRCLE_WORKFLOW_ID.txt" "$SH_MODULES"
+}" "$SH_MODULES_FILTERED" > /tmp/"$CIRCLE_WORKFLOW_ID.txt"
+mv /tmp/"$CIRCLE_WORKFLOW_ID.txt" "$SH_MODULES_FILTERED"
 
-xargs -a "$SH_MODULES" yq -y -s "reduce .[] as \$item ({}; . * \$item)" | tee "$SH_CONTINUE_CONFIG"
+xargs -a "$SH_MODULES_FILTERED" yq -y -s "reduce .[] as \$item ({}; . * \$item)" | tee "$SH_CONTINUE_CONFIG"
