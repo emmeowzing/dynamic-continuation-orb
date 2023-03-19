4a0ec8f90abd4092ff934e4194b412e673f4bfb5
Emma Doyle <emma@sbevision.com>
2023-03-18 23:47:00 -0400

    remove-modules-dependency: Auto-detect modules listing

--- src/scripts/filter.sh
+++ src/scripts/filter.sh
@@ -18,6 +18,11 @@ if [ -z "$_CIRCLE_TOKEN" ]; then
     exit 1
 fi
 
+if [ ! "$SH_AUTO_DETECT" ]; then
+    # We need to determine what the modules are, ignoring SH_MODULES
+    SH_MODULES="$(find .circleci/ -type f -name "*.yml" | grep -oP "(?<=.circleci/).*(?=.yml)")"
+fi
+
 # Add each module to `modules-filtered` if 1) `force-all` is set to `true`, or 2) there is a diff against master at HEAD, or 3) no workflow runs have occurred on the default branch for this project in the past $SH_REPORTING_WINDOW days.
 if [ ! "$SH_FORCE_ALL" ] || { [ "$SH_REPORTING_WINDOW" != "" ] && [ "$(curl -s -X GET --url "https://circleci.com/api/v2/insights/${SH_PROJECT_TYPE}/${_CIRCLE_ORGANIZATION}/${CIRCLE_PROJECT_REPONAME}/workflows?reporting-window=${SH_REPORTING_WINDOW}" --header "Circle-Token: ${SH_CIRCLE_TOKEN}" | jq -r "[ .items[].name ] | length")" -eq "0" ]; }; then
     printf "Running all workflows.\\n"
