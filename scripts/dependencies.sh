ba6ded62a56886efe56690a171a90de08789cce0
Emma Doyle <emma@premiscale.com>
2023-03-21 10:10:14 -0400

    fix-auto-detection: root-config not adequately handled (#38)
    * fix-auto-detection: root-config not adequately handled
    
    * fix-auto-detection: No need for escape
    
    * fix-auto-detection: Add braces
    
    ---------
    
    Co-authored-by: Emma Doyle <emma@sbevision.com>

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
-asdf plugin add nodejs   https://github.com/asdf-vm/asdf-nodejs.git
-asdf plugin-add yarn     https://github.com/twuni/asdf-yarn.git
+asdf plugin add nodejs      https://github.com/asdf-vm/asdf-nodejs.git
+asdf plugin-add yarn        https://github.com/twuni/asdf-yarn.git
+asdf plugin add shellcheck  https://github.com/luizm/asdf-shellcheck.git
