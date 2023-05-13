f45745cf9c5219f3f6b24e3aac5d079101217e3f
Emma Doyle <emma@sbevision.com>
2023-05-13 06:09:23 -0400

    fix-docs: undo that change

--- src/scripts/filter.sh
+++ src/scripts/filter.sh
@@ -129,9 +129,11 @@ else
                 touch ".circleci/${SH_ROOT_CONFIG}.ignore"
             fi
 
-            if [ "$CIRCLE_BRANCH" = "$SH_DEFAULT_BRANCH" ] && { [ "$SH_FORCE_ALL" -eq 1 ] || [ "$(git diff-tree --no-commit-id --name-only -r HEAD~"$SH_SQUASH_MERGE_LOOKBEHIND" "$SH_DEFAULT_BRANCH" . | awk NF | wildmatch -c ".circleci/$SH_ROOT_CONFIG.ignore")" != "" ] || { [ "$(git diff-tree --no-commit-id --name-only -r HEAD~"$SH_SQUASH_MERGE_LOOKBEHIND" "$SH_DEFAULT_BRANCH" ".circleci/$SH_ROOT_CONFIG.yml" | awk NF)" != "" ] && [ "$SH_INCLUDE_CONFIG_CHANGES" -eq 1 ]; }; }; then
-                printf "%s\\n" "$module_dots" >> "$SH_MODULES_FILTERED"
-                info "including \"/$module_slashes\" corresponding workflow \".circleci/${module_dots}.yml\""
+            if [ "$CIRCLE_BRANCH" = "$SH_DEFAULT_BRANCH" ]; then
+                if [ "$SH_FORCE_ALL" -eq 1 ] || [ "$(git diff-tree --no-commit-id --name-only -r HEAD~"$SH_SQUASH_MERGE_LOOKBEHIND" "$SH_DEFAULT_BRANCH" . | awk NF | wildmatch -c ".circleci/$SH_ROOT_CONFIG.ignore")" != "" ] || { [ "$(git diff-tree --no-commit-id --name-only -r HEAD~"$SH_SQUASH_MERGE_LOOKBEHIND" "$SH_DEFAULT_BRANCH" ".circleci/$SH_ROOT_CONFIG.yml" | awk NF)" != "" ] && [ "$SH_INCLUDE_CONFIG_CHANGES" -eq 1 ]; }; then
+                    printf "%s\\n" "$module_dots" >> "$SH_MODULES_FILTERED"
+                    info "including \"/$module_slashes\" corresponding workflow \".circleci/${module_dots}.yml\""
+                fi
             else
                 if [ "$SH_FORCE_ALL" -eq 1 ] || [ "$(git diff-tree --no-commit-id --name-only -r HEAD "$SH_DEFAULT_BRANCH" . | awk NF | wildmatch -c ".circleci/$SH_ROOT_CONFIG.ignore")" != "" ] || { [ "$(git diff-tree --no-commit-id --name-only -r HEAD "$SH_DEFAULT_BRANCH" ".circleci/$SH_ROOT_CONFIG.yml" | awk NF)" != "" ] && [ "$SH_INCLUDE_CONFIG_CHANGES" -eq 1 ]; }; then
                     printf "%s\\n" "$module_dots" >> "$SH_MODULES_FILTERED"
