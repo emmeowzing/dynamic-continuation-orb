dfe58bb0226a7d4c42315e498f9297bb051c4fa6
Emma Doyle <emma@sbevision.com>
2023-05-13 06:04:51 -0400

    fix-docs: add some debug steps

--- src/scripts/filter.sh
+++ src/scripts/filter.sh
@@ -170,11 +170,13 @@ IGNORE
         if [ "$CIRCLE_BRANCH" = "$SH_DEFAULT_BRANCH" ]; then
             if [ "$SH_FORCE_ALL" -eq 1 ] || [ "$(git diff-tree --no-commit-id --name-only -r HEAD~"$SH_SQUASH_MERGE_LOOKBEHIND" "$SH_DEFAULT_BRANCH" . | awk NF | wildmatch -c ".circleci/${module_dots}.ignore")" != "" ] || { [ "$(git diff-tree --no-commit-id --name-only -r HEAD~"$SH_SQUASH_MERGE_LOOKBEHIND" "$SH_DEFAULT_BRANCH" .circleci/"$module_dots".yml | awk NF)" != "" ] && [ "$SH_INCLUDE_CONFIG_CHANGES" -eq 1 ]; }; then
                 printf "%s\\n" "$module_dots" >> "$SH_MODULES_FILTERED"
+                info "Here 1"
                 info "including \"/$module_slashes\" corresponding workflow \".circleci/${module_dots}.yml\""
             fi
         else
             if [ "$SH_FORCE_ALL" -eq 1 ] || [ "$(git diff-tree --no-commit-id --name-only -r HEAD "$SH_DEFAULT_BRANCH" . | awk NF | wildmatch -c ".circleci/${module_dots}.ignore")" != "" ] || { [ "$(git diff-tree --no-commit-id --name-only -r HEAD "$SH_DEFAULT_BRANCH" .circleci/"$module_dots".yml | awk NF)" != "" ] && [ "$SH_INCLUDE_CONFIG_CHANGES" -eq 1 ]; }; then
                 printf "%s\\n" "$module_dots" >> "$SH_MODULES_FILTERED"
+                info "Here 2"
                 info "including \"/$module_slashes\" corresponding workflow \".circleci/${module_dots}.yml\""
             fi
         fi
